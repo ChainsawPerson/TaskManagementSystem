2594123eb4c9d2c8e83aa547b45b54768c563f3f
ChainsawPerson <takis.n.stamatopoulos@gmail.com>
2025-02-24 15:53:28 +0200

    Finalizing

--- taskmansys/src/main/java/module-info.java
+++ taskmansys/src/main/java/module-info.java
@@ -1,11 +1,8 @@
-module com.taskmansys {
+open module com.taskmansys {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.json;
     requires transitive javafx.graphics;
-    
-    opens com.taskmansys.gui to javafx.fxml;
-    opens com.taskmansys to javafx.fxml;
     
     exports com.taskmansys;
     exports com.taskmansys.gui;
